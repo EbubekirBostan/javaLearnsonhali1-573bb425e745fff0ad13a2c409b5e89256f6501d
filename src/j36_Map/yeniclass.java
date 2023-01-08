3dbd63cdfe70158bc8d08ecc22d8da235c0343f2
qaEbubekirBstn <bostanebubekir@gmail.com>
2023-01-08 17:23:29 +0300

    added conflig

--- src/j36_Map/yeniclass.java
+++ src/j36_Map/yeniclass.java
@@ -4,5 +4,7 @@ public class yeniclass {
     public static void main(String[] args) {
         System.out.println("fsfsfs");
         System.out.println("aasfklşskaf");
+        System.out.println("fsfsfs");
+        System.out.println("aasfklşskaf");
     }
 }
